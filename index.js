5329c27cf493dfa6798a67829db5384ad0ad58fe
jerviscen <jerviscen@tenvine.com>
2019-05-07 11:45:28 +0800

    [MOD] 部分修改

--- index.js
+++ index.js
@@ -21,23 +21,19 @@ app.get('/', function (req, res) {
 });
 
 
-// 引入所需要的第三方包
-
-
 let hotNews = [];                                // 热点新闻
 let localNews = [];                              // 本地新闻
 
 /**
  * index.js
- * [description] - 使用superagent.get()方法来访问百度新闻首页
+ * [description] - 使用superagent.get()方法来访
  */
 superagent.get(targetUrl).end((err, res) => {
   if (err) {
     // 如果访问失败或者出错，会这行这里
     console.log(`数据抓取失败 - ${err}`)
   } else {
-   // 访问成功，请求http://news.baidu.com/页面所返回的数据会包含在res
-   // 抓取热点新闻数据
+   // 访问成功，所返回的数据会包含在res
    console.log('看看爬取了什么', res)
    hotNews = getHotNews(res)
   }
